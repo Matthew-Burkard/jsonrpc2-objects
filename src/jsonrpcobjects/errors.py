1ad7c3b29261653689948c937899493f080634f4
Matthew Burkard <matthewburkard@protonmail.com>
2021-09-07 20:53:19 -0400

    feat: errors can be passed error data
    Clean error code and allow all errors to have data.

--- src/jsonrpcobjects/errors.py
+++ src/jsonrpcobjects/errors.py
@@ -1,8 +1,6 @@
 from typing import Type, Optional
 
-from pydantic import BaseModel
-
-from jsonrpcobjects.objects import ErrorObjectData, ErrorType
+from jsonrpcobjects.objects import ErrorObjectData, ErrorObject, ErrorType
 
 __all__ = (
     'PARSE_ERROR',
@@ -20,70 +18,49 @@ __all__ = (
     'get_exception_by_code',
 )
 
-
-class _ErrorData(BaseModel):
-    code: int
-    message: str
-
-
-PARSE_ERROR = _ErrorData(code=-32700, message='Parse error')
-INVALID_REQUEST = _ErrorData(code=-32600, message='Invalid Request')
-METHOD_NOT_FOUND = _ErrorData(code=-32601, message='Method not found')
-INVALID_PARAMS = _ErrorData(code=-32602, message='Invalid params')
-INTERNAL_ERROR = _ErrorData(code=-32603, message='Internal error')
+PARSE_ERROR = ErrorObject(code=-32700, message='Parse error')
+INVALID_REQUEST = ErrorObject(code=-32600, message='Invalid Request')
+METHOD_NOT_FOUND = ErrorObject(code=-32601, message='Method not found')
+INVALID_PARAMS = ErrorObject(code=-32602, message='Invalid params')
+INTERNAL_ERROR = ErrorObject(code=-32603, message='Internal error')
 
 
 class JSONRPCError(Exception):
-    def __init__(self, *args) -> None:
-        super(JSONRPCError, self).__init__(*args)
-
-
-class ParseError(JSONRPCError):
-    def __init__(self) -> None:
-        error = PARSE_ERROR
-        super(ParseError, self).__init__(
-            f'JSON-RPC Error: {error.code}: {error.message}',
-        )
-
-
-class InvalidRequest(JSONRPCError):
-    def __init__(self) -> None:
-        error = INVALID_REQUEST
-        super(InvalidRequest, self).__init__(
-            f'JSON-RPC Error: {error.code}: {error.message}',
-        )
-
-
-class MethodNotFound(JSONRPCError):
-    def __init__(self) -> None:
-        error = METHOD_NOT_FOUND
-        super(MethodNotFound, self).__init__(
-            f'JSON-RPC Error: {error.code}: {error.message}',
-        )
-
-
-class InvalidParams(JSONRPCError):
-    def __init__(self) -> None:
-        error = INVALID_PARAMS
-        super(InvalidParams, self).__init__(
-            f'JSON-RPC Error: {error.code}: {error.message}',
-        )
-
-
-class InternalError(JSONRPCError):
-    def __init__(self) -> None:
-        error = INTERNAL_ERROR
-        super(InternalError, self).__init__(
-            f'JSON-RPC Error: {error.code}: {error.message}',
-        )
-
-
-class ServerError(JSONRPCError):
     def __init__(self, error: ErrorType) -> None:
         msg = f'{error.code}: {error.message}'
         if isinstance(error, ErrorObjectData):
             msg += f'\nError Data: {error.data}'
-        super(ServerError, self).__init__(msg)
+        super(JSONRPCError, self).__init__(error)
+
+
+class ParseError(JSONRPCError):
+    def __init__(self) -> None:
+        super(ParseError, self).__init__(PARSE_ERROR)
+
+
+class InvalidRequest(JSONRPCError):
+    def __init__(self) -> None:
+        super(InvalidRequest, self).__init__(INVALID_REQUEST)
+
+
+class MethodNotFound(JSONRPCError):
+    def __init__(self) -> None:
+        super(MethodNotFound, self).__init__(METHOD_NOT_FOUND)
+
+
+class InvalidParams(JSONRPCError):
+    def __init__(self) -> None:
+        super(InvalidParams, self).__init__(INVALID_PARAMS)
+
+
+class InternalError(JSONRPCError):
+    def __init__(self) -> None:
+        super(InternalError, self).__init__(INTERNAL_ERROR)
+
+
+class ServerError(JSONRPCError):
+    def __init__(self, error: ErrorType) -> None:
+        super(ServerError, self).__init__(error)
 
 
 def get_exception_by_code(code: int) -> Optional[Type]:
